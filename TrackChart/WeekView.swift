1801309cdfad31b2737107eeed0a056e9adad1f9
Lennart Wisbar <lewisbar.git@gmail.com>
2025-09-15 16:31:49 +0200

    Add WeekView with heading and counter

---
+++ TrackChart/WeekView.swift
@@ -0,0 +1,21 @@
+//
+//  WeekView.swift
+//  TrackChart
+//
+//  Created by Lennart Wisbar on 15.09.25.
+//
+
+import SwiftUI
+
+struct WeekView: View {
+    var body: some View {
+        VStack {
+            Text("My Habit")
+            CounterView()
+        }
+    }
+}
+
+#Preview {
+    WeekView()
+}
