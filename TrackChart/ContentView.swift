b1b8a5af9255cbb896a5155333c0180e8a0c664c
Lennart Wisbar <lewisbar.git@gmail.com>
2025-09-15 16:06:28 +0200

    Initial Commit

---
+++ TrackChart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrackChart
+//
+//  Created by LennartWisbar on 15.09.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrackChart/TrackChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackChartApp.swift
+//  TrackChart
+//
+//  Created by LennartWisbar on 15.09.25.
+//
+
+import SwiftUI
+
+@main
+struct TrackChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
