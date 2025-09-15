aed9d5892014f8b9940fdb7e1fe7f4770ac0c3d5
Lennart Wisbar <lewisbar.git@gmail.com>
2025-09-15 17:56:49 +0200

    Add Week model

---
+++ TrackChart/Week.swift
@@ -0,0 +1,16 @@
+//
+//  Week.swift
+//  TrackChart
+//
+//  Created by Lennart Wisbar on 15.09.25.
+//
+
+struct Week {
+    let monday: Int
+    let tuesday: Int
+    let wednesday: Int
+    let thursday: Int
+    let friday: Int
+    let saturday: Int
+    let sunday: Int
+}
