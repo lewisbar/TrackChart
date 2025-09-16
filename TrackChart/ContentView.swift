1816be6bca7a9e8a29718612204a4b6bdae979f7
Lennart Wisbar <lewisbar.git@gmail.com>
2025-09-16 02:13:39 +0200

    Store data to UserDefaults on every change, and load it on appear

--- TrackChart/ContentView.swift
+++ TrackChart/ContentView.swift
@@ -8,25 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sampleValues = [1, 4, 3, 5, 2, 7, 8, 4, 6]
+    @Binding var values: [Int]
     private let title = "TrackChart"
 
     var body: some View {
-        MainView(title: title, values: sampleValues, submitNewValue: submitNewValue, deleteLastValue: deleteLastValue)
+        MainView(title: title, values: values, submitNewValue: submitNewValue, deleteLastValue: deleteLastValue)
             .padding()
     }
 
     private func submitNewValue(_ value: Int) {
-        sampleValues.append(value)
+        values.append(value)
     }
 
     private func deleteLastValue() {
-        guard !sampleValues.isEmpty else { return }
-        sampleValues.removeLast()
+        guard !values.isEmpty else { return }
+        values.removeLast()
     }
 }
 
 #Preview {
-    ContentView()
+    @Previewable @State var values = [1, 3, 2, 5, 4, 6, 9, 4]
+    
+    ContentView(values: $values)
 //        .environment(\.layoutDirection, .rightToLeft)
 }
--- TrackChart/TrackChartApp.swift
+++ TrackChart/TrackChartApp.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 @main
 struct TrackChartApp: App {
+    @State private var dataPoints: [Int] = []
+    private let userDefaultsKey = "DataPoints"
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(values: $dataPoints)
+                .onAppear {
+                    guard let loadedData = UserDefaults.standard.array(forKey: userDefaultsKey) as? [Int] else {
+                        print("No stored data found")
+                        return
+                    }
+
+                    dataPoints = loadedData
+                }
+                .onChange(of: dataPoints) {
+                    saveValues()
+                }
         }
+    }
+
+    private func saveValues() {
+        UserDefaults.standard.set(dataPoints, forKey: userDefaultsKey)
     }
 }
