3f5f272fdc73d6b68f6b9fd457f55e2290c2ed59
Lennart Wisbar <lewisbar.git@gmail.com>
2025-09-16 14:32:14 +0200

    Add empty lines for readability

--- TrackChart/Helpers/UIFont+maxCharacterWidth.swift
+++ TrackChart/Helpers/UIFont+maxCharacterWidth.swift
@@ -10,12 +10,14 @@ import SwiftUI
 extension UIFont {
     func maxCharacterWidth() -> CGFloat {
         let characters = "0123456789-" // Include digits and minus for negative numbers
+
         let maxWidth = characters.map { char in
             NSAttributedString(
                 string: String(char),
                 attributes: [.font: self]
             ).size().width
         }.max() ?? 0
+
         return ceil(maxWidth) // Round up for safety
     }
 }
