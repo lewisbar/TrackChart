a7acb2a37ad3a2bcb6ebe4046b64beedd41db0ce
Lennart Wisbar <lewisbar.git@gmail.com>
2025-09-16 17:33:09 +0200

    Add values to Store

--- StoreTests/StoreTests.swift
+++ StoreTests/StoreTests.swift
@@ -10,11 +10,26 @@ import Store
 
 class Store {
     private(set) var values: [Int] = []
+
+    func add(_ value: Int) {
+        values.append(value)
+    }
 }
 
 struct StoreTests {
     @Test func startsEmpty() {
         let sut = Store()
         #expect(sut.values == [])
+    }
+
+    @Test func add_appendsValue() {
+        let sut = Store()
+        #expect(sut.values == [])
+
+        sut.add(4)
+        #expect(sut.values == [4])
+
+        sut.add(2)
+        #expect(sut.values == [4, 2])
     }
 }
