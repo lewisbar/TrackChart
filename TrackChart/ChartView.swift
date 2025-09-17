849a037ccdab2a02a7050516bb92bbeb1e7fc84e
Lennart Wisbar <lewisbar.git@gmail.com>
2025-09-17 12:46:02 +0200

    Scroll to latest data on appear and when adding or removing values

--- TrackChart/ChartView.swift
+++ TrackChart/ChartView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import Charts
 
 struct ChartView: View {
-    private struct DataPoint: Identifiable {
+    private struct DataPoint: Identifiable, Equatable {
         let id = UUID()
         let value: Int
         let name: String
@@ -17,11 +17,12 @@ struct ChartView: View {
 
     private let dataPoints: [DataPoint]
 
-    @State private var xPosition: String?
+    @State private var xPosition = fallbackValue
     private let maxVisibleLength = 10
     private var visibleLength: Int { dataPoints.isEmpty ? 1 : max(0, min(totalPoints, maxVisibleLength)) }
-    private var homePosition: String { "\(max(0, totalPoints - visibleLength))" }
+    private var homePosition: String { dataPoints.last?.name ?? Self.fallbackValue }
     private var totalPoints: Int { dataPoints.count }
+    private static let fallbackValue = "0"
 
     init(values: [Int]) {
         self.dataPoints = values.enumerated().map { index, value in DataPoint(value: value, name: "\(index)") }
@@ -31,11 +32,12 @@ struct ChartView: View {
         Chart(dataPoints) { dataPoint in
             LineMark(x: .value("Data point", dataPoint.name), y: .value("Count", dataPoint.value))
         }
-        .chartXScale(domain: dataPoints.isEmpty ? ["0"] : dataPoints.map { $0.name })
+        .chartXScale(domain: dataPoints.isEmpty ? [Self.fallbackValue] : dataPoints.map { $0.name })
         .chartXVisibleDomain(length: visibleLength)
         .chartScrollableAxes(.horizontal)
-        .chartScrollPosition(initialX: xPosition ?? homePosition)
+        .chartScrollPosition(x: $xPosition)
         .onAppear { xPosition = homePosition }
+        .onChange(of: dataPoints) { xPosition = homePosition }
     }
 }
 
