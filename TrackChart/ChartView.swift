8406a7de7d0025bfed36841c22652d13d2b23869
Lennart Wisbar <lewisbar.git@gmail.com>
2025-09-17 11:35:52 +0200

    Enable horizontal scrolling

--- TrackChart/ChartView.swift
+++ TrackChart/ChartView.swift
@@ -17,6 +17,11 @@ struct ChartView: View {
 
     private let dataPoints: [DataPoint]
 
+    @State private var xPosition: String?
+    private let visibleLength = 10
+    private var homePosition: String { "\(max(0, totalPoints - visibleLength))" }
+    private var totalPoints: Int { dataPoints.count }
+
     init(values: [Int]) {
         self.dataPoints = values.enumerated().map { index, value in DataPoint(value: value, name: "\(index)") }
     }
@@ -25,6 +30,11 @@ struct ChartView: View {
         Chart(dataPoints) { dataPoint in
             LineMark(x: .value("Data point", dataPoint.name), y: .value("Count", dataPoint.value))
         }
+        .chartXScale(domain: dataPoints.map { $0.name })
+        .chartXVisibleDomain(length: visibleLength)
+        .chartScrollableAxes(.horizontal)
+        .chartScrollPosition(initialX: xPosition ?? homePosition)
+        .onAppear { xPosition = homePosition }
     }
 }
 
