4981df8be88e7beef5d73540246930bea736adcc
Lennart Wisbar <lewisbar.git@gmail.com>
2025-09-20 22:31:28 +0200

    Details can stack on top of each other

--- TrackChartTests/NavigationPathNavigatorTests.swift
+++ TrackChartTests/NavigationPathNavigatorTests.swift
@@ -36,4 +36,16 @@ struct NavigationPathNavigatorTests {
 
         #expect(sut.path == [.list, .detail(topic)])
     }
+
+    @Test func showDetail_whenAlreadyOnDetail_addsAnotherDetailToStack() {
+        let sut = NavigationPathNavigator()
+        let topic1 = Topic(id: UUID(), name: "a topic", entries: [1, 3, 5])
+        let topic2 = Topic(id: UUID(), name: "another topic", entries: [-1, -3])
+
+        sut.showDetail(for: topic1)
+        #expect(sut.path == [.list, .detail(topic1)])
+
+        sut.showDetail(for: topic2)
+        #expect(sut.path == [.list, .detail(topic1), .detail(topic2)])
+    }
 }
