29868b01447ac359a282ad54a72a2d0438319c55
Lennart Wisbar <lewisbar.git@gmail.com>
2025-09-20 22:28:50 +0200

    `showDetail` adds detail to path

--- TrackChartTests/NavigationPathNavigatorTests.swift
+++ TrackChartTests/NavigationPathNavigatorTests.swift
@@ -12,14 +12,28 @@ import TrackChart
 class NavigationPathNavigator {
     enum Destination: Equatable {
         case list
+        case detail(Topic)
     }
 
     var path: [Destination] = [.list]
+
+    func showDetail(for topic: Topic) {
+        path.append(.detail(topic))
+    }
 }
 
 struct NavigationPathNavigatorTests {
     @Test func init_startsWithTopicList() {
         let sut = NavigationPathNavigator()
         sut.path = [.list]
+    }
+
+    @Test func showDetail_addsDetailToStack() {
+        let sut = NavigationPathNavigator()
+        let topic = Topic(id: UUID(), name: "a topic", entries: [1, 3, 5])
+
+        sut.showDetail(for: topic)
+
+        #expect(sut.path == [.list, .detail(topic)])
     }
 }
