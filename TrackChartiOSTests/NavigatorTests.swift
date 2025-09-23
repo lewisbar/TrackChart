194e6c3b591810d268f418ee8d6daa9796a58c54
Lennart Wisbar <lewisbar.git@gmail.com>
2025-09-23 12:08:59 +0200

    Create and save new topic in Navigator

--- TrackChartiOSTests/NavigatorTests.swift
+++ TrackChartiOSTests/NavigatorTests.swift
@@ -7,18 +7,42 @@
 
 import Testing
 import Foundation
+import Domain
 
 struct NavigationTopic: Hashable, Codable {
     let id: UUID
     let name: String
     let entries: [Int]
+
+    init(id: UUID, name: String, entries: [Int]) {
+        self.id = id
+        self.name = name
+        self.entries = entries
+    }
+
+    init(from topic: Topic) {
+        self.id = topic.id
+        self.name = topic.name
+        self.entries = topic.entries
+    }
 }
 
 class Navigator {
     var path = [NavigationTopic]()
+    private let saveTopic: (Topic) -> Void
+
+    init(saveTopic: @escaping (Topic) -> Void) {
+        self.saveTopic = saveTopic
+    }
 
     func showDetail(for topic: NavigationTopic) {
         path.append(topic)
+    }
+
+    func showNewDetail() {
+        let newTopic = Topic(id: UUID(), name: "", entries: [])
+        saveTopic(newTopic)
+        path.append(NavigationTopic(from: newTopic))
     }
 
     func goBack() {
@@ -27,14 +51,14 @@ class Navigator {
     }
 }
 
-struct NavigatorTests {
+class NavigatorTests {
     @Test func init_startsWithHome() {
-        let sut = Navigator()
+        let sut = makeSUT()
         #expect(sut.path.isEmpty)
     }
 
     @Test func showDetail_addsDetailToStack() {
-        let sut = Navigator()
+        let sut = makeSUT()
         let topic = NavigationTopic(id: UUID(), name: "a topic", entries: [1, 3, 5])
 
         sut.showDetail(for: topic)
@@ -43,7 +67,7 @@ struct NavigatorTests {
     }
 
     @Test func showDetail_whenAlreadyOnDetail_addsAnotherDetailToStack() {
-        let sut = Navigator()
+        let sut = makeSUT()
         let topic1 = NavigationTopic(id: UUID(), name: "a topic", entries: [1, 3, 5])
         let topic2 = NavigationTopic(id: UUID(), name: "another topic", entries: [-1, -3])
 
@@ -54,8 +78,33 @@ struct NavigatorTests {
         #expect(sut.path == [topic1, topic2])
     }
 
+    @Test func showNewDetail_createsAndSavesNewTopic_andAddsDetailToStack() {
+        var savedTopics = [Topic]()
+        let sut = Navigator(saveTopic: { savedTopics.append($0) })
+
+        sut.showNewDetail()
+
+        #expect(savedTopics.count == 1)
+        #expect(savedTopics.first?.name == "")
+        #expect(savedTopics.first?.entries == [])
+        #expect(sut.path.count == 1)
+    }
+
+    @Test func showNewDetail_whenAlreadyOnDetail_createsAndSavesNewTopic_andAddsDetailToStack() {
+        var savedTopics = [Topic]()
+        let sut = Navigator(saveTopic: { savedTopics.append($0) })
+        sut.showDetail(for: NavigationTopic(id: UUID(), name: "a topic", entries: [1, 3, 5]))
+
+        sut.showNewDetail()
+
+        #expect(savedTopics.count == 1)
+        #expect(savedTopics.first?.name == "")
+        #expect(savedTopics.first?.entries == [])
+        #expect(sut.path.count == 2)
+    }
+
     @Test func goBack_removesDetailRepeatedly() {
-        let sut = Navigator()
+        let sut = makeSUT()
         let topic1 = NavigationTopic(id: UUID(), name: "a topic", entries: [1, 3, 5])
         let topic2 = NavigationTopic(id: UUID(), name: "another topic", entries: [-1, -3])
         sut.path = [topic1, topic2]
@@ -68,5 +117,19 @@ struct NavigatorTests {
 
         sut.goBack()
         #expect(sut.path == [])
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(saveTopic: @escaping (Topic) -> Void = { _ in }) -> Navigator {
+        let sut = Navigator(saveTopic: saveTopic)
+        weakSUT = sut
+        return sut
+    }
+
+    private weak var weakSUT: Navigator?
+
+    deinit {
+        #expect(weakSUT == nil, "Instance should have been deallocated. Potential memory leak.")
     }
 }
