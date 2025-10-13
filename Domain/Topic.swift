91e240c0a97177de6b84255e9cee47371cfaa1a0
Lennart Wisbar <lewisbar.git@gmail.com>
2025-10-13 17:21:01 +0200

    Add unsubmitted value to Topic

--- Domain/Topic.swift
+++ Domain/Topic.swift
@@ -11,10 +11,12 @@ public struct Topic: Equatable {
     public let id: UUID
     public let name: String
     public let entries: [Int]
+    public let unsubmittedValue: Int
 
-    public init(id: UUID, name: String, entries: [Int]) {
+    public init(id: UUID, name: String, entries: [Int], unsubmittedValue: Int = 0) {
         self.id = id
         self.name = name
         self.entries = entries
+        self.unsubmittedValue = unsubmittedValue
     }
 }
