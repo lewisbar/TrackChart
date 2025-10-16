0d75a7342c7fbd5a1d7a2ff47a72c20cd4b6cbec
Lennart Wisbar <lewisbar.git@gmail.com>
2025-10-16 22:38:42 +0200

    Preserve stored order of topics and entries

--- PersistenceTests/SwiftDataTopicPersistenceServiceTests.swift
+++ PersistenceTests/SwiftDataTopicPersistenceServiceTests.swift
@@ -26,14 +26,6 @@ private class TopicEntity {
         self.sortIndex = sortIndex
     }
 
-    init(from topic: Topic) {
-        self.id = topic.id
-        self.name = topic.name
-        self.entries = topic.entries.map(EntryEntity.init)
-        self.unsubmittedValue = topic.unsubmittedValue
-        self.sortIndex = 0  // TODO: Handle index logic
-    }
-
     var topic: Topic {
         Topic(id: id, name: name, entries: entries.map(\.entry), unsubmittedValue: unsubmittedValue)
     }
@@ -44,17 +36,12 @@ private class EntryEntity {
     var value: Double
     var timestamp: Date
     var sortIndex: Int
+    @Relationship(inverse: \TopicEntity.entries) var topic: TopicEntity?
 
     init(value: Double, timestamp: Date, sortIndex: Int) {
         self.value = value
         self.timestamp = timestamp
         self.sortIndex = sortIndex
-    }
-
-    init(from entry: Entry) {
-        self.value = entry.value
-        self.timestamp = entry.timestamp
-        self.sortIndex = 0  // TODO: Handle index logic
     }
 
     var entry: Entry {
@@ -71,7 +58,11 @@ class SwiftDataTopicPersistenceService {
     }
 
     func create(_ topic: Topic) throws {
-        let entity = TopicEntity(from: topic)
+        let entries = topic.entries.enumerated().map { index, entry in
+            EntryEntity(value: entry.value, timestamp: entry.timestamp, sortIndex: index)
+        }
+        let entity = TopicEntity(id: topic.id, name: topic.name, entries: entries, unsubmittedValue: topic.unsubmittedValue, sortIndex: sortedTopics.count)
+
         context.insert(entity)
         try context.save()
         sortedTopics.append(topic)
@@ -80,25 +71,65 @@ class SwiftDataTopicPersistenceService {
     func load() throws -> [Topic] {
         let descriptor = FetchDescriptor<TopicEntity>(sortBy: [SortDescriptor(\.sortIndex)])
         let entities = try context.fetch(descriptor)
-        return entities.map(\.topic)
+        sortedTopics = entities.map(\.topic)
+        return sortedTopics
     }
 }
 
 class SwiftDataTopicPersistenceServiceTests {
-    @Test func create_storesTopic() throws {
-        let container = try ModelContainer(
-            for: TopicEntity.self,
-            configurations: ModelConfiguration(isStoredInMemoryOnly: true)
-        )
-        let context = ModelContext(container)
-        let sut = SwiftDataTopicPersistenceService(context: context)
-        let topic = Topic(id: UUID(), name: "topic", entries: [
-            Entry(value: 2.3, timestamp: Date())
-        ], unsubmittedValue: 4.1)
+    @Test func createAndLoad_storesAndLoadsSingleTopic() throws {
+        let sut = try makeSUT()
+        let topic = topic(name: "topic")
 
         try sut.create(topic)
 
         let loadedTopics = try sut.load()
         #expect(loadedTopics == [topic])
+    }
+
+    @Test func createAndLoad_storesAndLoadsMultipleTopics_andPreservesOrder() throws {
+        let sut = try makeSUT()
+        let topic1 = topic(name: "topic 1")
+        let topic2 = topic(name: "topic 2")
+        let topic3 = topic(name: "topic 3")
+        let originalOrder = [topic2, topic1, topic3]
+
+        for topic in originalOrder {
+            try sut.create(topic)
+        }
+
+        let loadedTopics = try sut.load()
+        #expect(loadedTopics == originalOrder)
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT() throws -> SwiftDataTopicPersistenceService {
+        let context = try makeModelContext()
+        let sut = SwiftDataTopicPersistenceService(context: context)
+        weakSUT = sut
+        return sut
+    }
+
+    private func makeModelContext() throws -> ModelContext {
+        let container = try ModelContainer(
+            for: TopicEntity.self,
+            configurations: ModelConfiguration(isStoredInMemoryOnly: true)
+        )
+        return ModelContext(container)
+    }
+
+    private func topic(name: String = "a topic", values: [Double] = [], unsubmittedValue: Double = 0) -> Topic {
+        Topic(id: UUID(), name: name, entries: entries(from: values), unsubmittedValue: unsubmittedValue)
+    }
+
+    private func entries(from values: [Double]) -> [Entry] {
+        values.map { Entry(value: $0, timestamp: Date())}
+    }
+
+    private weak var weakSUT: SwiftDataTopicPersistenceService?
+
+    deinit {
+        #expect(weakSUT == nil, "Instance should have been deallocated. Potential memory leak.")
     }
 }
