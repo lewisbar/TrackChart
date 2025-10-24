e8fe7a27259978268e02169a4f568ad6c05cb8bc
Lennart Wisbar <lewisbar.git@gmail.com>
2025-10-24 18:18:02 +0200

    Move view model to separate file

---
+++ TrackChartiOS/DecimalInputViewModel.swift
@@ -0,0 +1,68 @@
+//
+//  DecimalInputViewModel.swift
+//  TrackChartiOS
+//
+//  Created by Lennart Wisbar on 24.10.25.
+//
+
+import Foundation
+
+@Observable
+class DecimalInputViewModel {
+    var value = "0"
+    private let submitValue: (Double) -> Void
+
+    init(submitValue: @escaping (Double) -> Void) {
+        self.submitValue = submitValue
+    }
+
+    func handleInput(_ key: String) {
+        switch key {
+        case "⌫":
+            if value.trimmingPrefix("-").count > 1 {
+                value.removeLast()
+            } else {
+                value = "0"
+            }
+        case ".":
+            if !value.contains(".") {
+                value += "."
+            }
+        default:
+            if value == "0" {
+                value = key
+            } else if value == "-0" {
+                value = "-" + key
+            } else {
+                value.append(key)
+            }
+        }
+    }
+
+    func toggleSign() {
+        if let number = Double(value) {
+            let endsWithDecimalPoint = value.hasSuffix(".")
+            let toggled = number * -1
+            var formattedNumber = formatNumberWithoutTrailingZeros(toggled)
+            if endsWithDecimalPoint {
+                formattedNumber.append(".")
+            }
+            value = formattedNumber
+        }
+    }
+
+    private func formatNumberWithoutTrailingZeros(_ number: Double) -> String {
+        if number.truncatingRemainder(dividingBy: 1) == 0 {
+            return String(format: "%.0f", number)
+        } else {
+            return String(number)
+        }
+    }
+
+    func submitNumber() {
+        if let value = Double(value) {
+            submitValue(value)
+        }
+        value = "0"
+    }
+}
