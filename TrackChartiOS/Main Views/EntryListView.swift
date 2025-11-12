91a9274bbbc5b58ebec5b09ca8ec9254eba616e8
Lennart Wisbar <lewisbar.git@gmail.com>
2025-11-12 12:19:51 +0100

    Add basic `EntryListView`

---
+++ TrackChartiOS/Main Views/EntryListView.swift
@@ -0,0 +1,51 @@
+//
+//  EntryListView.swift
+//  TrackChartiOS
+//
+//  Created by Lennart Wisbar on 12.11.25.
+//
+
+import SwiftUI
+
+struct ListEntry: Identifiable {
+    let id = UUID()
+    let value: Double
+    let timestamp: Date
+}
+
+struct EntryListView: View {
+    @Binding var entries: [ListEntry]
+
+    var body: some View {
+        List {
+            ForEach(entries) { entry in
+                entryCell(for: entry)
+            }
+        }
+    }
+
+    private func entryCell(for entry: ListEntry) -> some View {
+        HStack {
+            Text(entry.timestamp.formatted(date: .abbreviated, time: .shortened))
+                .font(.footnote)
+                .foregroundStyle(.secondary)
+            Spacer()
+            Text(entry.value.formatted(.number))
+        }
+    }
+}
+
+#Preview {
+    @Previewable @State var entries: [ListEntry] = [
+        .init(value: 2.1, timestamp: .now.advanced(by: -800)),
+        .init(value: 1, timestamp: .now.advanced(by: -700)),
+        .init(value: -1, timestamp: .now.advanced(by: -600)),
+        .init(value: -2.2, timestamp: .now.advanced(by: -500)),
+        .init(value: -3.4, timestamp: .now.advanced(by: -400)),
+        .init(value: 0, timestamp: .now.advanced(by: -300)),
+        .init(value: 2, timestamp: .now.advanced(by: -200)),
+        .init(value: 4, timestamp: .now.advanced(by: -100))
+    ]
+
+    EntryListView(entries: $entries)
+}
