1371223c86a4217062a0cba67a425f1eb7a89d2c
Lennart Wisbar <lewisbar.git@gmail.com>
2025-11-14 17:04:20 +0100

    Move `SwiftDataEntryListViewModel` to production

---
+++ TrackChartiOS/App Composition/SwiftData Wrapper Views/SwiftDataEntryListViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftDataEntryListViewModel.swift
+//  TrackChartiOS
+//
+//  Created by Lennart Wisbar on 14.11.25.
+//
+
+import Persistence
+
+class SwiftDataEntryListViewModel {
+    func listEntries(for topic: TopicEntity) -> [ListEntry] {
+        topic.sortedEntries.map(ListEntry.init)
+    }
+
+    func updateEntry(_ listEntry: ListEntry, of topic: TopicEntity) {
+        let entryEntity = topic.entries?.first(where: { $0.id == listEntry.id })
+        entryEntity?.value = listEntry.value
+        entryEntity?.timestamp = listEntry.timestamp
+    }
+
+    func deleteEntries(atOffsets offsets: IndexSet, from topic: TopicEntity) {
+        let ids = topic.sortedEntries.map(\.id)
+        let idsToDelete = offsets.map { ids[$0] }
+        for id in idsToDelete {
+            topic.entries?.removeAll(where: { $0.id == id })
+        }
+    }
+}
+
+private extension ListEntry {
+    init(from entryEntity: EntryEntity) {
+        self = ListEntry(id: entryEntity.id, value: entryEntity.value, timestamp: entryEntity.timestamp)
+    }
+}
--- TrackChartiOSTests/SwiftDataEntryListViewModelTests.swift
+++ TrackChartiOSTests/SwiftDataEntryListViewModelTests.swift
@@ -11,32 +11,6 @@ import Persistence
 @testable import TrackChartiOS
 import SwiftUI
 
-class SwiftDataEntryListViewModel {
-    func listEntries(for topic: TopicEntity) -> [ListEntry] {
-        topic.sortedEntries.map(ListEntry.init)
-    }
-
-    func updateEntry(_ listEntry: ListEntry, of topic: TopicEntity) {
-        let entryEntity = topic.entries?.first(where: { $0.id == listEntry.id })
-        entryEntity?.value = listEntry.value
-        entryEntity?.timestamp = listEntry.timestamp
-    }
-
-    func deleteEntries(atOffsets offsets: IndexSet, from topic: TopicEntity) {
-        let ids = topic.sortedEntries.map(\.id)
-        let idsToDelete = offsets.map { ids[$0] }
-        for id in idsToDelete {
-            topic.entries?.removeAll(where: { $0.id == id })
-        }
-    }
-}
-
-private extension ListEntry {
-    init(from entryEntity: EntryEntity) {
-        self = ListEntry(id: entryEntity.id, value: entryEntity.value, timestamp: entryEntity.timestamp)
-    }
-}
-
 class SwiftDataEntryListViewModelTests {
     @Test func listEntriesForTopic() throws {
         let entries = [
