dc49e22080fb511f672d3312b775bca0d3159a44
Lennart Wisbar <lewisbar.git@gmail.com>
2025-11-17 22:37:53 +0100

    Add hint on how to add a topic if topic list is empty; also improve bottom spacing to make room for the plus button without extending the List background

--- TrackChartiOS/Main Views/TopicListView.swift
+++ TrackChartiOS/Main Views/TopicListView.swift
@@ -23,13 +23,18 @@ struct TopicListView: View {
 
     private var list: some View {
         List {
+            if topics.isEmpty { addTopicHint }
+
             ForEach(topics) { topic in
                 TopicCell(topic: topic, showTopic: { showTopic(topic) })
                     .listRowSeparator(.hidden)
             }
             .onDelete(perform: deleteTopics)
             .onMove(perform: moveTopics)
-            Spacer()
+        }
+        .safeAreaInset(edge: .bottom) {
+            // Make room for the plus button
+            Color.clear.frame(height: 36)
         }
     }
 
@@ -41,26 +46,37 @@ struct TopicListView: View {
         }
         .accessibilityHint("Add a new topic")
     }
+
+    private var addTopicHint: some View {
+        Text(tutorial)
+            .foregroundColor(.secondary)
+            .padding()
+            .listRowSeparator(.hidden)
+    }
+
+    private let tutorial = """
+        Tap the plus button to add a topic. A topic can be anything you want to track, like daily pages read, pushups, your weight loss progress, or scientific data.
+        """
 }
 
 #Preview {
     let topics = [
         CellTopic(id: UUID(), name: "Topic 1", entries: [
-            .init(value: 2.1, timestamp: .now),
-            .init(value: 4, timestamp: .now),
-            .init(value: 3, timestamp: .now)
+            .init(value: 2.1, timestamp: .now.advanced(by: -300)),
+            .init(value: 4, timestamp: .now.advanced(by: -200)),
+            .init(value: 3, timestamp: .now.advanced(by: -100))
         ], aggregator: .sum, palette: .fire),
         CellTopic(id: UUID(), name: "Topic 2", entries: [
-            .init(value: 1, timestamp: .now),
-            .init(value: -4, timestamp: .now),
-            .init(value: 30, timestamp: .now)
+            .init(value: 1, timestamp: .now.advanced(by: -300)),
+            .init(value: -4, timestamp: .now.advanced(by: -200)),
+            .init(value: 30, timestamp: .now.advanced(by: -100))
         ], aggregator: .sum, palette: .forest),
         CellTopic(id: UUID(), name: "Topic 3", entries: [
-            .init(value: 2, timestamp: .now),
-            .init(value: 40, timestamp: .now),
-            .init(value: -13, timestamp: .now)
+            .init(value: 2, timestamp: .now.advanced(by: -300)),
+            .init(value: 40, timestamp: .now.advanced(by: -200)),
+            .init(value: -13, timestamp: .now.advanced(by: -100))
         ], aggregator: .sum, palette: .sunset),
     ]
     
-    TopicListView(topics: topics, deleteTopics: { _ in }, moveTopics: { _, _ in }, showTopic: { _ in }, createNewTopic: {})
+    TopicListView(topics: topics + topics, deleteTopics: { _ in }, moveTopics: { _, _ in }, showTopic: { _ in }, createNewTopic: {})
 }
