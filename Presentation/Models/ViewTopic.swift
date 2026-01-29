85fd2c4d657d873186945cc08a893d3f08e05176
Lennart Wisbar <lewisbar.git@gmail.com>
2026-01-29 14:17:29 +0100

    Add sum, average, highest, and lowest to `ViewTopic`

--- Presentation/Models/ViewTopic.swift
+++ Presentation/Models/ViewTopic.swift
@@ -13,15 +13,35 @@ public struct ViewTopic: Identifiable, Hashable {
     public let entries: [ViewEntry]
     public let aggregator: ViewAggregator
     public let treatsMissingAsZero: Bool
+    public let sum: Double
+    public let average: Double?
+    public let highest: Double?
+    public let lowest: Double?
     public let palette: Palette
 
-    public init(id: UUID, name: String, details: String, entries: [ViewEntry], aggregator: ViewAggregator, treatsMissingAsZero: Bool, palette: Palette) {
+    public init(
+        id: UUID,
+        name: String,
+        details: String,
+        entries: [ViewEntry],
+        aggregator: ViewAggregator,
+        treatsMissingAsZero: Bool,
+        sum: Double,
+        average: Double?,
+        highest: Double?,
+        lowest: Double?,
+        palette: Palette
+    ) {
         self.id = id
         self.name = name
         self.details = details
         self.entries = entries
         self.aggregator = aggregator
         self.treatsMissingAsZero = treatsMissingAsZero
+        self.sum = sum
+        self.average = average
+        self.highest = highest
+        self.lowest = lowest
         self.palette = palette
     }
 }
